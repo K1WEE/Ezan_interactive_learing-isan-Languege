6f70903ef1e7d4b791178a402731fa063cbb187c
K1WEE <kawin.pa@kkumail.com>
2025-03-26 21:32:26 +0700

    fix sound

--- ezan_project/urls.py
+++ ezan_project/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,7 +14,12 @@ urlpatterns = [
     path('api/', include('progress.urls')),
     path('progress/', include('progress.urls_views')),
     path('social-auth/', include('social_django.urls', namespace='social')),
+    
+    # เพิ่ม URL pattern สำหรับไฟล์ media ที่จะทำงานทั้งใน development และ production
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
+# ยังคงใช้ static function สำหรับ development แต่จะไม่มีผลในโหมด production
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
